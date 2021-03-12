ac9413be72e923064bdda50dc887cc9a816eb62c
dylan-rinella <rinelladylan@aim.com>
2021-03-12 11:08:48 -0500

    working on it

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,60 @@
-// Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  Projects.insert(req.body)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({message: `Project ${req.params.id} has been deleted`})
+      
+    })
+    .catch(next)
+})
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message, // DEV
+    stack: err.stack, // DEV
+    custom: 'something went terrible in the hubs router', // PRODUCTION
+  })
+})
+
+
+
+module.exports = router
+
--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const server = express();
 
+const projectsRouter = require('./projects/projects-router')
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Success!</h2>
+  `)
+})
+
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
